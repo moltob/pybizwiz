cc155cdedfa29e0aa20e8953d62c4cec158750ad
Mike Pagel <mike@mpagel.de>
2016-12-14 18:31:52 +0100

    moved bower components out of common folder

--- bwsite/settings.py
+++ bwsite/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'bower_components'),
+]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 CRISPY_FAIL_SILENTLY = not DEBUG
