3fec1f55c4e7f0f2f856c5edde71e810a3859300
Mike Pagel <mike@mpagel.de>
2016-12-15 20:08:52 +0100

    article list requires authenticated user

--- bizwiz/articles/views.py
+++ bizwiz/articles/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 
 from bizwiz.articles.models import Article
 
 
-class List(ListView):
+class List(LoginRequiredMixin, ListView):
     model = Article
