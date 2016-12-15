eceffa86615698e73adc825ec1243231ece03a6d
Mike Pagel <mike@mpagel.de>
2016-12-15 13:19:58 +0100

    moved templates

--- bizwiz/accounts/urls.py
+++ bizwiz/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 from bizwiz.accounts.forms import login_helper
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, {
-        'template_name': 'account/login.html',
+        'template_name': 'accounts/login.html',
         'extra_context': {'helper': login_helper},
     }, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
