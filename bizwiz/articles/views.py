b7e92a294c8d33eabc31325067650bbc87f0668f
Mike Pagel <mike@mpagel.de>
2016-12-19 13:00:17 +0100

    added ordering of table

--- bizwiz/articles/views.py
+++ bizwiz/articles/views.py
@@ -7,3 +7,20 @@ from bizwiz.articles.models import Article
 class List(LoginRequiredMixin, ListView):
     model = Article
     paginate_by = 15
+
+    @property
+    def order_by(self):
+        return self.request.GET.get('order_by', 'name')
+
+    @property
+    def order_dir(self):
+        return self.request.GET.get('order_dir', 'asc')
+
+    def get_ordering(self):
+        if self.order_dir == 'asc':
+            return self.order_by
+        else:
+            return '-{}'.format(self.order_by)
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(order_by=self.order_by, order_dir=self.order_dir)
