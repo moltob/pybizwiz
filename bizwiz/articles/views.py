a42e430cdb6191d89439a5be94e1f853b618b581
Mike Pagel <mike@mpagel.de>
2016-12-19 14:28:56 +0100

    refactored view part of pagination and ordering

--- bizwiz/articles/views.py
+++ bizwiz/articles/views.py
@@ -2,25 +2,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 
 from bizwiz.articles.models import Article
+from bizwiz.common.views import PaginatedOrderedListView
 
 
-class List(LoginRequiredMixin, ListView):
+class List(LoginRequiredMixin, PaginatedOrderedListView):
     model = Article
-    paginate_by = 15
-
-    @property
-    def order_by(self):
-        return self.request.GET.get('order_by', 'name')
-
-    @property
-    def order_dir(self):
-        return self.request.GET.get('order_dir', 'asc')
-
-    def get_ordering(self):
-        if self.order_dir == 'asc':
-            return self.order_by
-        else:
-            return '-{}'.format(self.order_by)
-
-    def get_context_data(self, **kwargs):
-        return super().get_context_data(order_by=self.order_by, order_dir=self.order_dir)
--- bizwiz/common/views.py
+++ bizwiz/common/views.py
@@ -1,3 +1,4 @@
+from django.views.generic import ListView
 from django.views.generic import TemplateView
 
 from bizwiz.version import BIZWIZ_VERSION
@@ -8,3 +9,24 @@ class Welcome(TemplateView):
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(version=BIZWIZ_VERSION, **kwargs)
+
+
+class PaginatedOrderedListView(ListView):
+    paginate_by = 15
+
+    @property
+    def order_by(self):
+        return self.request.GET.get('order_by', 'name')
+
+    @property
+    def order_dir(self):
+        return self.request.GET.get('order_dir', 'asc')
+
+    def get_ordering(self):
+        if self.order_dir == 'asc':
+            return self.order_by
+        else:
+            return '-{}'.format(self.order_by)
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(order_by=self.order_by, order_dir=self.order_dir)
