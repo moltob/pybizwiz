c27bf228c19a625a6aa59a6e5da7d51def29df3f
Mike Pagel <mike@mpagel.de>
2016-12-19 15:50:10 +0100

    renamed

--- bizwiz/exceptions.py
+++ bizwiz/exceptions.py
@@ -1,9 +1,10 @@
 """Bizwiz application exceptions."""
+from django.core.exceptions import ImproperlyConfigured
 
 
 class BizwizError(Exception):
     """Base class for all errors raised by Bizwiz."""
 
 
-class IncorrectViewConfigurationError(BizwizError, ValueError):
+class IncorrectViewConfigurationError(BizwizError, ImproperlyConfigured):
     """The view is not fully or correctly set up."""
