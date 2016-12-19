52a0078813c3e46373f92933924c97abb6d58a10
Mike Pagel <mike@mpagel.de>
2016-12-19 08:44:11 +0100

    added article to admin interface

--- bizwiz/articles/admin.py
+++ bizwiz/articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from bizwiz.articles.models import Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Article, ArticleAdmin)
--- bizwiz/articles/models.py
+++ bizwiz/articles/models.py
@@ -6,3 +6,6 @@ class Article(models.Model):
     name = models.CharField(max_length=128)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inactive = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
