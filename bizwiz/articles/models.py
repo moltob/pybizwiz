ba176fcee508fc60351ec6970fe21fb8afbb1abe
Mike Pagel <mike@mpagel.de>
2016-12-19 09:35:29 +0100

    article name now unique

--- bizwiz/articles/models.py
+++ bizwiz/articles/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Article(models.Model):
     """An article to be sold to customers."""
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, unique=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inactive = models.BooleanField(default=False)
 
