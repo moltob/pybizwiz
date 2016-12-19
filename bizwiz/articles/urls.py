be288e256a3db9d6cd63859a1a7349afd73e75a9
Mike Pagel <mike@mpagel.de>
2016-12-19 16:56:25 +0100

    allowed to toogle inactive article display

--- bizwiz/articles/urls.py
+++ bizwiz/articles/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from bizwiz.articles import views
 
 urlpatterns = [
-    url(r'^$', views.List.as_view(), name='list'),
+    url(r'^$', views.List.as_view(), {'show_inactive': False}, name='list'),
+    url(r'^all/$', views.List.as_view(), {'show_inactive': True}, name='list_with_inactive'),
 ]
--- bizwiz/articles/views.py
+++ bizwiz/articles/views.py
@@ -9,3 +9,12 @@ class List(LoginRequiredMixin, OrderedListViewMixin, ListView):
     model = Article
     ordering = 'name'
     paginate_by = 15
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(show_inactive=self.kwargs['show_inactive'], **kwargs)
+
+    def get_queryset(self):
+        self.queryset = Article.objects.all()
+        if not self.kwargs['show_inactive']:
+            self.queryset = self.queryset.filter(inactive=False)
+        return super().get_queryset()
