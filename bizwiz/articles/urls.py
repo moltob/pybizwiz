bc1df5dcddd3ab533bb0b493df07227559ac0dea
Mike Pagel <mike@mpagel.de>
2016-12-20 19:40:39 +0100

    improved URL design and translation

--- bizwiz/articles/urls.py
+++ bizwiz/articles/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from bizwiz.articles import views
 
 urlpatterns = [
-    url(r'^$', views.List.as_view(), {'show_inactive': False}, name='list'),
-    url(r'^all/$', views.List.as_view(), {'show_inactive': True}, name='list_with_inactive'),
+    url(r'^active/$', views.List.as_view(), {'show_inactive': False}, name='list_active'),
+    url(r'^$', views.List.as_view(), {'show_inactive': True}, name='list'),
     url(r'^edit/(?P<pk>[0-9]+)/$', views.Update.as_view(), name='update'),
 ]
