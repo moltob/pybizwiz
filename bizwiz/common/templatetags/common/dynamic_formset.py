32d63fdea4b5f389ef1b4741eee019c50fdb5ddb
Mike Pagel <mike@mpagel.de>
2016-12-21 19:21:52 +0100

    refactored dynamic formset automation into reusable tags and templates

---
+++ bizwiz/common/templatetags/common/dynamic_formset.py
@@ -0,0 +1,28 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('common/dynamic_formset_begin.html')
+def dynformset(css_id, formset):
+    return dict(css_id=css_id, formset=formset)
+
+
+@register.inclusion_tag('common/dynamic_formset_body.html')
+def dynformset_body(formset):
+    return dict(formset=formset)
+
+
+@register.inclusion_tag('common/dynamic_formset_add_button.html')
+def dynformset_add_button():
+    return {}
+
+
+@register.inclusion_tag('common/dynamic_formset_end.html')
+def enddynformset():
+    return {}
+
+
+@register.inclusion_tag('common/dynamic_formset_apply.html')
+def dynformset_apply(selector):
+    return dict(selector=selector)
