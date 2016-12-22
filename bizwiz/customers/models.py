d733aaf85cc290eca8b1cf87f30dc81ec7c7f92f
Mike Pagel <mike@mpagel.de>
2016-12-22 11:28:36 +0100

    initial customer model

--- bizwiz/customers/models.py
+++ bizwiz/customers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    title = models.CharField(max_length=20, blank=True)
+    company_name = models.CharField(max_length=50, blank=True)
+    street_address = models.CharField(max_length=50, blank=True)
+    zip_code = models.CharField(max_length=20, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    phone_number = models.CharField(max_length=50, blank=True)
+    mobile_number = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(max_length=50, blank=True)
+    notes = models.TextField(blank=True)
