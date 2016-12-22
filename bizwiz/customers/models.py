33ce8f97bf0d4365b94cfcb23c3055f18b8523a7
Mike Pagel <mike@mpagel.de>
2016-12-22 12:03:54 +0100

    annotated verbose names in customer model

--- bizwiz/customers/models.py
+++ bizwiz/customers/models.py
@@ -1,15 +1,61 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class Customer(models.Model):
-    last_name = models.CharField(max_length=50)
-    first_name = models.CharField(max_length=50)
-    title = models.CharField(max_length=20, blank=True)
-    company_name = models.CharField(max_length=50, blank=True)
-    street_address = models.CharField(max_length=50, blank=True)
-    zip_code = models.CharField(max_length=20, blank=True)
-    city = models.CharField(max_length=50, blank=True)
-    phone_number = models.CharField(max_length=50, blank=True)
-    mobile_number = models.CharField(max_length=50, blank=True)
-    email = models.EmailField(max_length=50, blank=True)
-    notes = models.TextField(blank=True)
+    last_name = models.CharField(
+        _("Last name"),
+        max_length=50
+    )
+    first_name = models.CharField(
+        _("First name"),
+        max_length=50
+    )
+    title = models.CharField(
+        _("Title"),
+        max_length=20,
+        blank=True
+    )
+    company_name = models.CharField(
+        _("Company name"),
+        max_length=50,
+        blank=True
+    )
+    street_address = models.CharField(
+        _("Street address"),
+        max_length=50,
+        blank=True
+    )
+    zip_code = models.CharField(
+        _("Zip code"),
+        max_length=20,
+        blank=True
+    )
+    city = models.CharField(
+        _("City"),
+        max_length=50,
+        blank=True
+    )
+    phone_number = models.CharField(
+        _("Phone number"),
+        max_length=50,
+        blank=True
+    )
+    mobile_number = models.CharField(
+        _("Mobile number"),
+        max_length=50,
+        blank=True
+    )
+    email = models.EmailField(
+        _("Email address"),
+        max_length=50,
+        blank=True
+    )
+    notes = models.TextField(
+        _("Notes"),
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = _("Customer")
+        verbose_name_plural = _("Customers")
