4c7b39ea511a5cd7f169e30f3765f417696baa77
Mike Pagel <mike@mpagel.de>
2016-12-22 22:17:10 +0100

    list of customers

--- bizwiz/customers/urls.py
+++ bizwiz/customers/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
-from bizwiz.articles import views
+from bizwiz.customers import views
 
 urlpatterns = [
-    #url(r'^$', views.List.as_view(), {'show_inactive': True}, name='list'),
+    url(r'^$', views.List.as_view(), name='list'),
     #url(r'^active/$', views.List.as_view(), {'show_inactive': False}, name='list_active'),
     #url(r'^create/$', views.Create.as_view(), name='create'),
     #url(r'^edit/(?P<pk>[0-9]+)/$', views.Update.as_view(), name='update'),
--- bizwiz/customers/views.py
+++ bizwiz/customers/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import mixins
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from bizwiz.common.views import OrderedListViewMixin
+from bizwiz.customers.models import Customer
+
+
+class List(mixins.LoginRequiredMixin, OrderedListViewMixin, generic.ListView):
+    model = Customer
+    ordering = 'last_name'
+    paginate_by = 15
