eab7eeac55b58b380cf145a86e46ee6e5b75d8b4
Mike Pagel <mike@mpagel.de>
2016-12-22 11:46:35 +0100

    customers admin

--- bizwiz/customers/admin.py
+++ bizwiz/customers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from bizwiz.customers.models import Customer
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Customer, CustomerAdmin)
