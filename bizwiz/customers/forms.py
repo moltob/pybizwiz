229b279694bd54f99de3c9dd875b4f195418b524
Mike Pagel <mike@mpagel.de>
2017-01-04 11:58:59 +0100

    switched customer form to grid layout

--- bizwiz/customers/forms.py
+++ bizwiz/customers/forms.py
@@ -9,30 +9,44 @@ CostumerForm = forms.modelform_factory(Customer, fields='__all__')
 
 class UpdateForm(CostumerForm):
     helper = helper.FormHelper()
-    helper.form_class = 'form-horizontal'
-    helper.label_class = 'col-lg-2'
-    helper.field_class = 'col-lg-8'
+    #helper.form_class = 'form-horizontal'
+    #helper.label_class = 'col-lg-2'
+    #helper.field_class = 'col-lg-8'
     helper.layout = layout.Layout(
-        'salutation',
-        'title',
-        'first_name',
-        'last_name',
-        'company_name',
+        layout.Row(
+            layout.Div('salutation', css_class='col-lg-3'),
+            layout.Div('title', css_class='col-lg-3'),
+        ),
+        layout.Row(
+            layout.Div('first_name', css_class='col-lg-6'),
+            layout.Div('last_name', css_class='col-lg-6'),
+        ),
+        layout.Row(
+            layout.Div('company_name', css_class='col-lg-6'),
+        ),
         layout.Fieldset(
             _("Address"),
-            'street_address',
-            'zip_code',
-            'city',
+            layout.Row(
+                layout.Div('street_address', css_class='col-lg-6'),
+            ),
+            layout.Row(
+                layout.Div('zip_code', css_class='col-lg-2'),
+                layout.Div('city', css_class='col-lg-4'),
+            ),
         ),
         layout.Fieldset(
             _("Contact data"),
-            'phone_number',
-            'mobile_number',
-            'email',
+            layout.Row(
+                layout.Div('phone_number', css_class='col-lg-4'),
+                layout.Div('mobile_number', css_class='col-lg-4'),
+                layout.Div('email', css_class='col-lg-4'),
+            ),
         ),
         layout.Fieldset(
             _("Additional"),
-            'notes',
+            layout.Row(
+                layout.Div('notes', css_class='col-lg-12'),
+            ),
         ),
     )
     helper.add_input(layout.Submit('submit', _("Submit")))
