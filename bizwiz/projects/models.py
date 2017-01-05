c9173da464b990ec84b24274b4cd3d5c7de45914
Mike Pagel <mike@mpagel.de>
2017-01-05 13:54:21 +0100

    initialized project model

--- bizwiz/projects/models.py
+++ bizwiz/projects/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from bizwiz.customers.models import Customer
+
+
+class Project(models.Model):
+    """Photography project."""
+    name = models.CharField(
+        _("Name"),
+        max_length=50,
+        unique=True,
+    )
+    start_date = models.DateField(
+        _("Start date"),
+        auto_now=True,
+    )
+    notes = models.TextField(
+        _("Notes"),
+        blank=True
+    )
+
+
+class CustomerGroup(models.Model):
+    """Groups of customers within a project."""
+    name = models.CharField(
+        _("Name"),
+        max_length=50,
+        unique=True,
+    )
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    customers = models.ManyToManyField(Customer)
