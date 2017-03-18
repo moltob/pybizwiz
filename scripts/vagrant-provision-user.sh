6a185d8513212be769359d106dd4289e2ce25ee6
Mike Pagel <mike@mpagel.de>
2017-03-18 13:16:17 +0100

    added new scripts to vagrant VM

--- scripts/vagrant-provision-user.sh
+++ scripts/vagrant-provision-user.sh
@@ -5,6 +5,7 @@ set -x
 # ---- Utilities ----
 mkdir -p bin
 cat /vagrant/scripts/deploy.sh | fromdos > bin/deploy.sh
+cat /vagrant/scripts/docker-run.sh | fromdos > bin/docker-run.sh
 
 echo export PATH=~/bin:$PATH >> ~/.bashrc
 chmod a+x bin/*
