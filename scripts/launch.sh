c6e5bfd35b2dcff34cdc39c669417c2a8f3b0815
Mike Pagel <mike@mpagel.de>
2017-03-18 13:36:55 +0100

    signal propagation

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -2,6 +2,9 @@
 set -e
 set -x
 
+# propagate SIGTERM to gunicorn
+trap "ps h --ppid $BASHPID -o pid | xargs kill -TERM" SIGTERM
+
 python manage.py migrate
 gunicorn --bind 0.0.0.0:443 \
           --access-logfile data/gunicorn-access.log \
