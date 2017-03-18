6c97f0a04f21a679799eafd071b2e0eb8f115fac
Mike Pagel <mike@mpagel.de>
2017-03-18 14:19:36 +0100

    starting gunicorn inplace in shell process

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -2,14 +2,10 @@
 set -e
 set -x
 
-# propagate SIGTERM to gunicorn
-echo "ps h --ppid $$ -o pid | xargs kill -TERM"
-trap "ps h --ppid $$ -o pid | xargs kill -TERM" SIGTERM
-
 python manage.py migrate
-gunicorn --bind 0.0.0.0:443 \
-          --access-logfile data/gunicorn-access.log \
-          --error-logfile data/gunicorn-error.log \
-          --keyfile data/bizwiz.key \
-          --certfile data/bizwiz.cert \
-          bwsite.wsgi
+exec gunicorn --bind 0.0.0.0:443 \
+              --access-logfile data/gunicorn-access.log \
+              --error-logfile data/gunicorn-error.log \
+              --keyfile data/bizwiz.key \
+              --certfile data/bizwiz.cert \
+              bwsite.wsgi
