c027599948b15bd4000ce2353f3d92b07d671297
Mike Pagel <mike@mpagel.de>
2017-08-23 11:27:43 +0200

    replaced deprecated Django authentication views

--- bizwiz/accounts/urls.py
+++ bizwiz/accounts/urls.py
@@ -7,9 +7,9 @@ from bizwiz.accounts.forms import login_helper
 app_name = 'accounts'
 
 urlpatterns = [
-    url(r'^login/$', auth_views.login, {
-        'template_name': 'accounts/login.html',
-        'extra_context': {'helper': login_helper},
-    }, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^login/$', auth_views.LoginView.as_view(
+        template_name='accounts/login.html',
+        extra_context={'helper': login_helper},
+    ), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
 ]
