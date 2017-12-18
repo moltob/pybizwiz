a5bcd383a8c171c4e9bd4187ab063953895037fb
Mike Pagel <mike@mpagel.de>
2017-12-18 11:55:14 +0100

    fixed url path migration typo

--- bizwiz/invoices/urls.py
+++ bizwiz/invoices/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('edit/<int:pk>/', views.Update.as_view(), name='update'),
     path('print/invoice-<int:number>.pdf', views.Print.as_view(), name='print'),
     path('sales/', views.Sales.as_view(), name='sales'),
-    path('sales/<int:year>/$', views.List.as_view(), {'subset': 'year_paid'}, name='sales'),
-    path('sales/<int:year>/articles/$', views.ArticleSales.as_view(), name='sales_articles'),
+    path('sales/<int:year>/', views.List.as_view(), {'subset': 'year_paid'}, name='sales'),
+    path('sales/<int:year>/articles/', views.ArticleSales.as_view(), name='sales_articles'),
 ]
