305201ccbd076971eb7dabc4f6cdd793d0002f79
Mike Pagel <mike@mpagel.de>
2018-01-04 17:13:31 +0100

    cleaned up

--- scripts/update-container.bat
+++ scripts/update-container.bat
@@ -6,7 +6,10 @@ set SCRIPT=update-container.sh
 pushd %LOCAL%
 
 dos2unix -n %SCRIPT% ux-%SCRIPT%
+
 scp ux-%SCRIPT% admin@diskstation:~/scripts
 ssh -t admin@diskstation ~/scripts/ux-%SCRIPT%
+
+del /Q ux-%SCRIPT%
 
 popd
--- scripts/update-container.sh
+++ scripts/update-container.sh
@@ -7,4 +7,4 @@ export PATH=$PATH:/usr/local/bin
 sudo docker rm -f bizwiz || true
 sudo docker image pull mpagel/bizwiz:latest
 sudo docker run -v /volume1/docker/bizwiz/data:/app/bizwiz/data -p 4443:443 --name bizwiz mpagel/bizwiz:latest &
-sleep 10
+sleep 60
